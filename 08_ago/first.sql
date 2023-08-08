77ab1ef60d69e72018ea8d1482d63d0808ed834f
Irving Suarez <irving.dev@icloud.com>
2023-08-08 17:33:03 -0600

    [08_ago]: Adding customer_id to 1st e

--- 08_ago/first.sql
+++ 08_ago/first.sql
@@ -2,7 +2,7 @@
 1. Create a query that gets the payment data from each client. The required data is: ID, name, last name, amount, date
 */
 
-SELECT payment_id, first_name, last_name, amount, payment_date
+SELECT c.customer_id, payment_id, first_name, last_name, amount, payment_date
 FROM payment
-         FULL JOIN customer a on a.customer_id = payment.customer_id
+         FULL JOIN customer c on c.customer_id = payment.customer_id
 ;
