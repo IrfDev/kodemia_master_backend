dcafc10f2b945f70de88846acf88f847594c8c27
Irving Suarez <irving.dev@icloud.com>
2023-08-08 17:26:28 -0600

    [08_ago]: Adding instructions to 1st ex

--- 08_ago/exercises.md
+++ 08_ago/exercises.md
@@ -1,6 +1,5 @@
 ## Instructions
 
-
 1. Create a query that gets the payment data from each client. The required data is: ID, name, last name, amount, date
 
 
--- 08_ago/first.sql
+++ 08_ago/first.sql
@@ -1,3 +1,7 @@
+/*
+1. Create a query that gets the payment data from each client. The required data is: ID, name, last name, amount, date
+*/
+
 SELECT payment_id, first_name, last_name, amount, payment_date
 FROM payment
          FULL JOIN customer a on a.customer_id = payment.customer_id
